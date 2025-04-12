7e9ff71949dcc47fcb5267c3c3b3add8abcc07a3
khamri.Achraf <achrafkhamri99@gmail.com>
2025-04-12 23:27:35 +0100

    add sidebar decorators

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,20 +1,81 @@
-import React from 'react'
-import styles from '@/styles/Sidebar.module.css'
+import React from "react";
+import styles from "@/styles/Sidebar.module.css";
 import NewMessageButton from "../assets/new-message.svg";
-import EmailIcon from '../assets//icons/email.svg';
+import EmailIcon from "../assets//icons/email.svg";
+import SelectedDecorator from "../assets//icons/selectedDecHeavy.svg";
+import Decorator from "../assets//icons/decHeavy.svg";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
+const sidebarItems = [
+  {
+    title: "Inbox",
+    path: "/",
+    count: 170,
+  },
+  {
+    title: "Drafts",
+    path: "/drafts",
+    count: 0,
+  },
+  {
+    title: "Sent",
+    path: "/sent",
+    count: 0,
+  },
+  {
+    title: "All Mail",
+    path: "/all",
+    count: 220,
+  },
+  {
+    title: "Spam",
+    path: "/spam",
+    count: 0,
+  },
+  {
+    title: "Trash",
+    path: "/trash",
+    count: 0,
+  },
+  {
+    title: "Important",
+    path: "/important",
+    count: 16,
+  },
+  {
+    title: "Starred",
+    path: "/starred",
+    count: 0,
+  },
+];
 
 const Sidebar = () => {
-    return (
-        <aside className={styles.sidebar}>
-            <NewMessageButton className={styles.button} />
-            <div className={styles.email}>
-                <EmailIcon className={styles.icon} />
-                <p>juxtopposed@skyrim.tmrl</p>
-            </div>
+  const router = useRouter();
 
-        </aside>
-    )
-}
+  return (
+    <aside className={styles.sidebar}>
+      <NewMessageButton className={styles.button} />
+      
+      <div className={styles.email}>
+        <EmailIcon className={styles.icon} />
+        <p>achraf@skyrim.tmrl</p>
+      </div>
+      {sidebarItems.map(({ title, path, count }) => (
+        <Link href={path} key={title}>
+          <div
+            className={`${styles.sidebarItem} ${
+              router.pathname === path && styles.active
+            }`}
+          >
+            <div className={styles.title}>{title} {count > 0 && <span>({count})</span>}</div>
+            {router.pathname === path ? <SelectedDecorator className={styles.SelectedDecorator} /> : <Decorator className={styles.SelectedDecorator} />}
+          </div>
+        </Link>
+      ))}
+      <div className={styles.filler}></div>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,26 @@
 import "@/styles/globals.css";
 import Layout from "../../components/Layout";
+import localFont from "next/font/local";
+
+const futura = localFont({
+  src: [
+    {
+      path: "../fonts/futura.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/futura_bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-futura",
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <Layout className={futura.className}>
       <Component {...pageProps} />
     </Layout>
   );
